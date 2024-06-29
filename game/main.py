7e36f80f51f372519f877d09af4e871060abcab6
mikoedit <miko@mikoedit.com>
2024-06-29 09:36:23 +0200

    main: suggest gameloop

--- game/main.py
+++ game/main.py
@@ -1,7 +1,6 @@
 import pygame
 
 pygame.init()
-
 screen = pygame.display.set_mode([500, 500])
 
 running = True
@@ -11,10 +10,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
+    #GameLoop?
+    #   Intro Screen
+    #   Create Character
+    #   StartLocationEvent
+    #   Map Screen
+    #       -> LocationEvent
+    #           -> CheckPlayHealth/Sanity
+    #               -> Show Death Screen
+    #               -> Save Death
+
     screen.fill((255, 255, 255))
-
     pygame.draw.circle(screen, (0, 0, 255), (250, 250),75)
-
     pygame.display.flip()
 
-pygame.quit()
+pygame.quit()      
