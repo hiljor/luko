edf729caae0b54352d333667ea98d55ed093ac83
mikoedit <miko@mikoedit.com>
2024-06-29 22:19:30 +0200

    main: messing with pydraw and fonts

--- game/main.py
+++ game/main.py
@@ -11,6 +11,8 @@ current_map = mapFactory.new_map(5, 50, 2)
 
 running = True
 hidden = True
+bg_colour = (30, 30, 30)
+fg_colour = (0, 0, 30)
 while running:
 
     for event in pygame.event.get():
@@ -31,15 +33,17 @@ while running:
     #               -> Show Death Screen
     #               -> Save Death
 
-    screen.fill((30, 30, 30))
+    screen.fill(bg_colour)
     font = pygame.font.Font('game/PixelScript.ttf', 20)
-    hello_text = font.render("Hello, World",1,(200,200,200))
+    logo_text = font.render("Luko Games",1,(200,200,200))
+    hello_text = font.render("Hidden",1,(200,200,200))
     current_map.render(screen)
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250),75)
-    pygame.draw.circle(screen, (0, 0, 0), (250, 250),50)
+    pygame.draw.circle(screen, (150, 120, 0), (250, 250),75)
+    pygame.draw.circle(screen, bg_colour, (225, 250),55)
+    screen.blit(logo_text,(255,250))
     if hidden == False:
-        pygame.draw.circle(screen, (0, 0, 255), (250, 250),75)
-        screen.blit(hello_text,(250,250))
+        pygame.draw.rect(screen, (0, 0, 255), (250, 450),75)
+        screen.blit(hello_text,(250,450))
 
     pygame.display.flip()
 
